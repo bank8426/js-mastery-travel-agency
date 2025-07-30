838d5c6bb2edf655854f65d09dffa86607365f63
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-07-30 16:30:59 +0700

    install dayjs, add utils.ts, add NavItems component, use NavItems in admin-layout

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
 import { Outlet } from "react-router";
+import { NavItems } from "componets";
 
 const AdminLayout = () => {
   return (
     <div className="admin-layout">
       Momile
-      <aside className="w-full max-w-[270px] hidden lg:block">Sidebar</aside>
+      <aside className="w-full max-w-[270px] hidden lg:block">
+        <SidebarComponent width={270} enableGestures={false}>
+          <NavItems />
+        </SidebarComponent>
+      </aside>
       <aside className="children">
         <Outlet />
       </aside>
