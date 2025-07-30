f7c26c43fb84be43462ade2778348dd832e0ca5d
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-07-30 17:00:39 +0700

    add MobileSidebar component, add handleClick in NavItems to close Sidebar when click on mobile

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
 import { Outlet } from "react-router";
-import { NavItems } from "componets";
+import { MobileSidebar, NavItems } from "componets";
 
 const AdminLayout = () => {
   return (
     <div className="admin-layout">
-      Momile
+      <MobileSidebar />
       <aside className="w-full max-w-[270px] hidden lg:block">
         <SidebarComponent width={270} enableGestures={false}>
           <NavItems />
