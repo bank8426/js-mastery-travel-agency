9229f583fe704c0f6c4553c60f97b923f98542e6
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-07-30 16:39:19 +0700

    add footer in NavItems.tsx

--- componets/NavItems.tsx
+++ componets/NavItems.tsx
@@ -4,6 +4,12 @@ import { Link, NavLink } from "react-router";
 import { sidebarItems } from "~/constants";
 
 const NavItems = () => {
+  const user = {
+    name: "Gu",
+    email: "gubankcpe.dev@gmail.com",
+    imageUrl: "/assets/images/david.webp",
+  };
+
   return (
     <section className="nav-items">
       <Link to="/" className="link-logo">
@@ -32,6 +38,30 @@ const NavItems = () => {
             </NavLink>
           ))}
         </nav>
+
+        <footer className="nav-footer">
+          <img
+            src={user?.imageUrl || "/assets/images/david.webp"}
+            alt={user?.name || "David"}
+          />
+          <article>
+            <h2>{user?.name}</h2>
+            <p>{user?.email}</p>
+          </article>
+
+          <button
+            onClick={() => {
+              console.log("logout");
+            }}
+            className="cursor-pointer"
+          >
+            <img
+              src="/assets/icons/logout.svg"
+              alt="logout"
+              className="size-6"
+            />
+          </button>
+        </footer>
       </div>
     </section>
   );
