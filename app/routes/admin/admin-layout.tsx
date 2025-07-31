bedb008b6dad0fdc202e50b0a5d1911512b8b40e
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-07-31 16:38:43 +0700

    add sign-in route, add sign-in page, update admin-layout to handle user session and validate user with admin role

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,8 +1,31 @@
 import React from "react";
 import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
-import { Outlet } from "react-router";
+import { Outlet, redirect } from "react-router";
 import { MobileSidebar, NavItems } from "componets";
+import { account } from "~/appwrite/client";
+import { getExistingUser, storeUserData } from "~/appwrite/auth";
 
+export async function clientLoader() {
+  try {
+    const user = await account.get();
+
+    if (!user.$id) return redirect("/sign-in");
+
+    const existingUser = await getExistingUser(user.$id);
+
+    if (existingUser?.status === "user") {
+      console.log("normal user access admin dashboard, redirect to home");
+
+      return redirect("/");
+    }
+
+    return existingUser?.$id ? existingUser : await storeUserData();
+  } catch (error) {
+    console.log("Error in clientLoader", error);
+
+    return redirect("/sign-in");
+  }
+}
 const AdminLayout = () => {
   return (
     <div className="admin-layout">
