798ca3a27e817017a79590db99e848184dd036e1
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-07-31 17:26:27 +0700

    install @sentry/react-router, @sentry/profiling-node, add SENTRY_AUTH_TOKEN in .env, setup sentry

---
+++ instrument.server.mjs
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/react-router";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://7acd76d24870744279c6a716d32760ca@o4509756416655360.ingest.de.sentry.io/4509756430745680",
+
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+
+  integrations: [nodeProfilingIntegration()],
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  profilesSampleRate: 1.0, // profile every transaction
+});
